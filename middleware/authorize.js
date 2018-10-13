de92f1f33f17dd092e0d316d5dff5633798d4967
Alexandre Charoy <alexandre.charoy@gmail.com>
2018-10-13 14:26:03 +0200

    Fixed indentation

--- middleware/authorize.js
+++ middleware/authorize.js
@@ -4,24 +4,24 @@ const config = require('config');
 //Express middleware that authorizes a route using the JWT token supplied in the authorization header
 function authorize(req, res, next) {
 
-        var authHeader = req.headers['authorization'];
+    var authHeader = req.headers['authorization'];
 
-        if (authHeader) {
+    if (authHeader) {
 
-            //Verify token
-            var token = authHeader.substr(authHeader.indexOf(" ") + 1);
-            jwt.verify(token, config.get('secret'), function(error, decoded) {
-                if (error) {
-                    res.status(401).end();
-                } else {
-                    req.token = decoded;
-                    next();
-                }
-            });
+        //Verify token
+        var token = authHeader.substr(authHeader.indexOf(" ") + 1);
+        jwt.verify(token, config.get('secret'), function(error, decoded) {
+            if (error) {
+                res.status(401).end();
+            } else {
+                req.token = decoded;
+                next();
+            }
+        });
 
-        } else {
-            res.status(401).end();
-        }
+    } else {
+        res.status(401).end();
+    }
 }
 
 module.exports = AuthModule;
