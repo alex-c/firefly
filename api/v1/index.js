600945d33bcfb46e066b2b7e23cda007dcfb54f1
Alexandre Charoy <alexandre.charoy@gmail.com>
2018-10-13 15:24:26 +0200

    Cleanup

--- api/v1/index.js
+++ api/v1/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-//router.get('/auth', require('./auth'));
 router.use('/login', require('./login.js'));
 router.use('/users', require('./users.js'));
 router.use('/accounts', require('./accounts.js'));
