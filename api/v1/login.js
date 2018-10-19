b4855e4ddecfa913e52f96663451231b1d7e5878
Alexandre Charoy <alexandre.charoy@gmail.com>
2018-10-19 16:09:33 +0200

    Added Insomnia workspace export

--- api/v1/login.js
+++ api/v1/login.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 //Load User model
 const User = require('../../models/User.js');
 
-//Login route
+//Authentication route
 async function login(req, res, next) {
 
     if (req.body.name && req.body.password) {
