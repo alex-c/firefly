3f087596ce3a0b15cb396684a5fe13f7a6d63d81
Alexandre Charoy <alexandre.charoy@gmail.com>
2020-03-17 13:13:23 +0100

    Added Element UI, i18n and themes

--- firefly-frontend/src/App.vue
+++ firefly-frontend/src/App.vue
@@ -1,12 +1,64 @@
 <template>
-  <div id="app">WIP</div>
+  <div id="app">
+    <div class="header">Theme Preview</div>
+    <el-row>
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+    <el-row>
+      <el-button plain>Plain</el-button>
+      <el-button type="primary" plain>Primary</el-button>
+      <el-button type="success" plain>Success</el-button>
+      <el-button type="info" plain>Info</el-button>
+      <el-button type="warning" plain>Warning</el-button>
+      <el-button type="danger" plain>Danger</el-button>
+    </el-row>
+    <el-row>
+      <el-button round>Round</el-button>
+      <el-button type="primary" round>Primary</el-button>
+      <el-button type="success" round>Success</el-button>
+      <el-button type="info" round>Info</el-button>
+      <el-button type="warning" round>Warning</el-button>
+      <el-button type="danger" round>Danger</el-button>
+    </el-row>
+    <el-row>
+      <el-button icon="el-icon-search" circle></el-button>
+      <el-button type="primary" icon="el-icon-edit" circle></el-button>
+      <el-button type="success" icon="el-icon-check" circle></el-button>
+      <el-button type="info" icon="el-icon-message" circle></el-button>
+      <el-button type="warning" icon="el-icon-star-off" circle></el-button>
+      <el-button type="danger" icon="el-icon-delete" circle></el-button>
+    </el-row>
+  </div>
 </template>
 
 <style lang="scss">
+@import './style/themes.scss';
+
+body {
+  background-color: var($--theme-background);
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  color: var($--theme-foreground);
+}
+
+.header {
+  background-color: var($--theme-primary);
+  padding: 18px;
+  font-size: 24px;
+}
+
+.el-row {
+  margin-top: 12px;
 }
 </style>
--- firefly-frontend/src/main.js
+++ firefly-frontend/src/main.js
@@ -1,12 +1,40 @@
 import Vue from 'vue';
-import App from './App.vue';
+Vue.config.productionTip = false;
+
+// Router & Vuex
 import router from './router';
 import store from './store';
 
-Vue.config.productionTip = false;
+// Element UI & theme
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/reset.css';
+import './style/themes.scss';
+import enLocale from 'element-ui/lib/locale/lang/en';
+Vue.use(ElementUI);
 
+// Internationalization lib
+import VueI18n from 'vue-i18n';
+Vue.use(VueI18n);
+
+// Load messages
+import enMessages from './i18n/en.json';
+const messages = {
+  en: Object.assign(enMessages, enLocale),
+};
+
+// Configure internationalization
+let language = localStorage.getItem('language') || 'en';
+const i18n = new VueI18n({
+  locale: language,
+  fallbackLocale: 'en',
+  messages,
+});
+
+// Mount app
+import App from './App.vue';
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App),
 }).$mount('#app');
