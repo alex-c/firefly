1a3ba5a67e4e4906894c66d61b99989933def5d7
Alexandre Charoy <alexandre.charoy@gmail.com>
2020-03-24 14:51:37 +0100

    WIP on user admin

--- firefly-frontend/src/views/admin-pages/UserAdmin.vue
+++ firefly-frontend/src/views/admin-pages/UserAdmin.vue
@@ -1,12 +1,28 @@
 <template>
-  <div id="view-user-admin">User Administration</div>
+  <div id="view-user-admin">
+    <Breadcrumb :items="[{name: 'Administration'}, {name: 'Users'}]" />
+    <Box>
+      <template #header>Users</template>
+      <span>test</span>
+    </Box>
+  </div>
 </template>
 
 <script>
+import Breadcrumb from '@/components/Breadcrumb.vue';
+import Box from '@/components/Box.vue';
+
 export default {
   name: 'user-admin',
+  components: { Breadcrumb, Box },
 };
 </script>
 
 <style lang="scss" scoped>
+#view-user-admin {
+  padding: 0px 16px;
+  & > div {
+    margin-top: 16px;
+  }
+}
 </style>
